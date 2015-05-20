994ac584bc90611d254ac6690e6ee605cb87afc9
aburkhalter512 <aburkhalter512@gmail.com>
2015-05-20 12:24:17 -0700

    Updating source file locations.

--- code/example/runtestBark.bat
+++ code/example/runtestBark.bat
@@ -3,4 +3,4 @@ REM parameters: ImageDirectory NumImages imagenames... NumDescriptors descriptor
 REM The number of homographies should be one less than the number of images.
 REM (Each homography is with respect to the first image.)
 
-start /w ../Debug/Program4.exe ./images/bark/ 3 img1.ppm img2.ppm img3.ppm 2 SIFT SURF H1to2p.txt H1to3p.txt
+start /w ../Debug/example.exe ./images/bark/ 3 img1.ppm img2.ppm img3.ppm 2 SIFT SURF H1to2p.txt H1to3p.txt
